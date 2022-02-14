42e375bbcdedcf4136c71ae3f544e7d416f5a599
cjaverliat <charles.javerliat@gmail.com>
2022-02-14 11:26:29 +0100

    fix: Fix XREcho instance being destroyed

--- Assets/XREcho/Scripts/XREcho.cs
+++ Assets/XREcho/Scripts/XREcho.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 [RequireComponent(typeof(XREchoConfig))]
 public class XREcho : MonoBehaviour
 {
