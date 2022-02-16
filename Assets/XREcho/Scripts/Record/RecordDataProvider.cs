79afd18d1cad7f2513b8fe590fbc35419a82f535
cjaverliat <charles.javerliat@gmail.com>
2022-02-16 13:58:57 +0100

    feature: Aggregated heatmap

--- Assets/XREcho/Scripts/Record/RecordDataProvider.cs
+++ Assets/XREcho/Scripts/Record/RecordDataProvider.cs
@@ -1,18 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class RecordDataProvider : MonoBehaviour, IRecordDataProvider
 {
     
     private ReplayManager _replayManager;
+    private XREchoConfig _config;
     
     private void Start()
     {
         _replayManager = ReplayManager.GetInstance();
+        _config = XREchoConfig.GetInstance();
     }
     
-    public void LoadRecordData(out List<Vector3> positions, out List<float> timestamps, out float totalRecordTime)
+    public void LoadAllProjectRecordsData(out List<List<Vector3>> positions, out List<List<float>> timestamps, out List<float> totalDurations)
+    {
+        var projectFolder = Path.Combine(_config.GetRecordingsFolder(), _config.project);
+        var projectDirectory = new DirectoryInfo(projectFolder);
+        var sessionsDirectories = projectDirectory.GetDirectories();
+        
+        var recordIdx = 0;
+        positions = new List<List<Vector3>>();
+        timestamps = new List<List<float>>();
+        totalDurations = new List<float>();
+        
+        foreach(var sessionDirectory in sessionsDirectories)
+        {
+            var objectsDataFiles = sessionDirectory.GetFiles("*objectsData" + _config.GetFileSuffix() + ".csv");
+            
+            foreach (var objectsDataFile in objectsDataFiles)
+            {
+                var data = CSVReader.ReadCSV(objectsDataFile.FullName);
+                var totalDuration = Convert.ToSingle(data[data.Count - 1]["timestamp"]);
+                
+                positions.Add(new List<Vector3>());
+                timestamps.Add(new List<float>());
+                totalDurations.Add(totalDuration);
+
+                foreach (var entry in data)
+                {
+                    var actionId = Convert.ToInt32(entry["actionId"]);
+
+                    // Only get position of the HMD
+                    if (actionId != 0) continue;
+                    
+                    positions[recordIdx].Add(ReplayManager.LoadPosition(entry));
+                    timestamps[recordIdx].Add(Convert.ToSingle(entry["timestamp"]));
+                }
+
+                ++recordIdx;
+            }
+        }
+    }
+
+    public void LoadSelectedRecordData(out List<Vector3> positions, out List<float> timestamps, out float recordDuration)
     {
         positions = new List<Vector3>();
         timestamps = new List<float>();
@@ -22,17 +65,18 @@ public class RecordDataProvider : MonoBehaviour, IRecordDataProvider
             throw new InvalidOperationException("Can't compute position heatmap without recordings or if recording is empty");
         }
 
-        var logs = _replayManager.objectsData[0];
+        var data = _replayManager.objectsData[0];
+        recordDuration = Convert.ToSingle(data[data.Count - 1]["timestamp"]);
 
-        foreach (var entry in logs)
+        foreach (var entry in data)
         {
-            if ((int)(float)entry["actionId"] == (int) ActionType.POSITION)
+            var actionId = Convert.ToInt32(entry["actionId"]);
+
+            if (actionId == 0)
             {
                 positions.Add(ReplayManager.LoadPosition(entry));
-                timestamps.Add((float) entry["timestamp"]);
+                timestamps.Add(Convert.ToSingle(entry["timestamp"]));
             }
         }
-
-        totalRecordTime = _replayManager.GetTotalReplayTime();
     }
 }
--- Assets/XREcho/Scripts/UI/GUIHeatmap.cs
+++ Assets/XREcho/Scripts/UI/GUIHeatmap.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using System.Globalization;
 using UnityEditor;
 using UnityEngine;
 
 public class GUIHeatmap
 {
-    private PositionHeatmapManager _positionHeatmapManager;
-
-    private GUIStylesManager _stylesManager;
+    private readonly PositionHeatmapManager _positionHeatmapManager;
+    private readonly GUIStylesManager _stylesManager;
+    
     private bool _displayHeatmap;
+    private bool _displayAggregatedHeatmap;
 
     private string _heatmapScaleLowerBoundStr;
     private string _heatmapScaleUpperBoundStr;
@@ -44,6 +44,22 @@ public class GUIHeatmap
         if (showPositionHeatmap)
         {
             GUILayout.BeginVertical("box");
+
+            var showAggregatedHeatmap = GUILayout.Toggle(_displayAggregatedHeatmap, "Aggregate all records", _stylesManager.toggleStyle);
+
+            if (showAggregatedHeatmap != _displayAggregatedHeatmap)
+            {
+                _displayAggregatedHeatmap = showAggregatedHeatmap;
+                
+                if(showAggregatedHeatmap)
+                    EditorUtility.DisplayProgressBar("Generating aggregated heatmap", "Generating aggregated position heatmap texture...", -1);
+                else
+                    EditorUtility.DisplayProgressBar("Generating heatmap", "Generating position heatmap texture...", -1);
+                
+                _positionHeatmapManager.ToggleAggregatedPositionHeatmap(showAggregatedHeatmap);
+                
+                EditorUtility.ClearProgressBar();
+            }
             
             GUILayout.Label("Heatmap transparency");
             var transparencyValue = GUILayout.HorizontalSlider(_lastHeatmapTransparency, 0f, 1f);
@@ -54,75 +70,95 @@ public class GUIHeatmap
                 _lastHeatmapTransparency = transparencyValue;
             }
 
-            var heatmapScaleLowerBoundStyle = new GUIStyle(GUI.skin.textField);
-            var heatmapScaleUpperBoundStyle = new GUIStyle(GUI.skin.textField);
-            
-            var lowerBoundValid = float.TryParse(_heatmapScaleLowerBoundStr, out var heatmapScaleLowerBound) && Math.Abs(heatmapScaleLowerBound) <= 10e-3;
-            var upperBoundValid = float.TryParse(_heatmapScaleUpperBoundStr, out var heatmapScaleUpperBound) && Math.Abs(heatmapScaleUpperBound - _positionHeatmapManager.GetMaxDuration()) <= 10e-3 && heatmapScaleUpperBound > heatmapScaleLowerBound;
-
-            heatmapScaleLowerBoundStyle.normal.textColor = lowerBoundValid ? Color.white : Color.red;
-            heatmapScaleUpperBoundStyle.normal.textColor = upperBoundValid ? Color.white : Color.red;
-            heatmapScaleLowerBoundStyle.hover.textColor = lowerBoundValid ? Color.white : Color.red;
-            heatmapScaleUpperBoundStyle.hover.textColor = upperBoundValid ? Color.white : Color.red;
-            heatmapScaleLowerBoundStyle.focused.textColor = lowerBoundValid ? Color.white : Color.red;
-            heatmapScaleUpperBoundStyle.focused.textColor = upperBoundValid ? Color.white : Color.red;
-            heatmapScaleLowerBoundStyle.stretchWidth = true;
-            heatmapScaleUpperBoundStyle.stretchWidth = true;
-
-            var minMaxBtnStyle = new GUIStyle(GUI.skin.button)
+            if (showAggregatedHeatmap)
             {
-                fixedWidth = 50
-            };
-
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("Scale lower bound (s): ", GUI.skin.label);
-            _heatmapScaleLowerBoundStr = GUILayout.TextField(_heatmapScaleLowerBoundStr, heatmapScaleLowerBoundStyle);
-            var setLowerScaleToMin = GUILayout.Button("Min", minMaxBtnStyle);
-            GUILayout.EndHorizontal();
-            
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("Scale upper bound (s): ", GUI.skin.label);
-            _heatmapScaleUpperBoundStr = GUILayout.TextField(_heatmapScaleUpperBoundStr, heatmapScaleUpperBoundStyle);
-            var setUpperScaleToMax = GUILayout.Button("Max", minMaxBtnStyle);
-            GUILayout.EndHorizontal();
-
-            if (setLowerScaleToMin)
-            {
-                _heatmapScaleLowerBoundStr = "0";
-            }
-
-            if (setUpperScaleToMax)
-            {
-                _heatmapScaleUpperBoundStr = $"{_positionHeatmapManager.GetMaxDuration():0.###}";
-            }
-
-            if (lowerBoundValid && upperBoundValid)
-            {
-                heatmapScaleLowerBound /= _positionHeatmapManager.GetMaxDuration();
-                heatmapScaleUpperBound /= _positionHeatmapManager.GetMaxDuration();
-                
-                if (Math.Abs(heatmapScaleLowerBound - _lastHeatmapScaleLowerBound) > 10e-6 || Math.Abs(heatmapScaleUpperBound - _lastHeatmapScaleUpperBound) > 10e-6)
+                if (GUILayout.Button("Force re-generate heatmap"))
                 {
-                    _positionHeatmapManager.SetScaleBounds(heatmapScaleLowerBound, heatmapScaleUpperBound);
-                    _lastHeatmapScaleLowerBound = heatmapScaleLowerBound;
-                    _lastHeatmapScaleUpperBound = heatmapScaleUpperBound;
+                    EditorUtility.DisplayProgressBar("Generating aggregated heatmap", "Generating aggregated position heatmap texture...", -1);
+                    _positionHeatmapManager.ForceRegenerate();
+                    EditorUtility.ClearProgressBar();
+                }
+            }
+            else
+            {
+                var heatmapScaleLowerBoundStyle = new GUIStyle(GUI.skin.textField);
+                var heatmapScaleUpperBoundStyle = new GUIStyle(GUI.skin.textField);
+
+                var lowerBoundValid = float.TryParse(_heatmapScaleLowerBoundStr, out var heatmapScaleLowerBound) &&
+                                      Math.Abs(heatmapScaleLowerBound) <= 10e-3;
+                var upperBoundValid = float.TryParse(_heatmapScaleUpperBoundStr, out var heatmapScaleUpperBound) &&
+                                      Math.Abs(heatmapScaleUpperBound - _positionHeatmapManager.GetMaxDuration()) <=
+                                      10e-3 && heatmapScaleUpperBound > heatmapScaleLowerBound;
+
+                heatmapScaleLowerBoundStyle.normal.textColor = lowerBoundValid ? Color.white : Color.red;
+                heatmapScaleUpperBoundStyle.normal.textColor = upperBoundValid ? Color.white : Color.red;
+                heatmapScaleLowerBoundStyle.hover.textColor = lowerBoundValid ? Color.white : Color.red;
+                heatmapScaleUpperBoundStyle.hover.textColor = upperBoundValid ? Color.white : Color.red;
+                heatmapScaleLowerBoundStyle.focused.textColor = lowerBoundValid ? Color.white : Color.red;
+                heatmapScaleUpperBoundStyle.focused.textColor = upperBoundValid ? Color.white : Color.red;
+                heatmapScaleLowerBoundStyle.stretchWidth = true;
+                heatmapScaleUpperBoundStyle.stretchWidth = true;
+
+                var minMaxBtnStyle = new GUIStyle(GUI.skin.button)
+                {
+                    fixedWidth = 50
+                };
+
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("Scale lower bound (s): ", GUI.skin.label);
+                _heatmapScaleLowerBoundStr =
+                    GUILayout.TextField(_heatmapScaleLowerBoundStr, heatmapScaleLowerBoundStyle);
+                var setLowerScaleToMin = GUILayout.Button("Min", minMaxBtnStyle);
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("Scale upper bound (s): ", GUI.skin.label);
+                _heatmapScaleUpperBoundStr =
+                    GUILayout.TextField(_heatmapScaleUpperBoundStr, heatmapScaleUpperBoundStyle);
+                var setUpperScaleToMax = GUILayout.Button("Max", minMaxBtnStyle);
+                GUILayout.EndHorizontal();
+
+                if (setLowerScaleToMin)
+                {
+                    _heatmapScaleLowerBoundStr = "0";
+                }
+
+                if (setUpperScaleToMax)
+                {
+                    _heatmapScaleUpperBoundStr = $"{_positionHeatmapManager.GetMaxDuration():0.###}";
+                }
+
+                if (lowerBoundValid && upperBoundValid)
+                {
+                    heatmapScaleLowerBound /= _positionHeatmapManager.GetMaxDuration();
+                    heatmapScaleUpperBound /= _positionHeatmapManager.GetMaxDuration();
+
+                    if (Math.Abs(heatmapScaleLowerBound - _lastHeatmapScaleLowerBound) > 10e-6 ||
+                        Math.Abs(heatmapScaleUpperBound - _lastHeatmapScaleUpperBound) > 10e-6)
+                    {
+                        _positionHeatmapManager.SetScaleBounds(heatmapScaleLowerBound, heatmapScaleUpperBound);
+                        _lastHeatmapScaleLowerBound = heatmapScaleLowerBound;
+                        _lastHeatmapScaleUpperBound = heatmapScaleUpperBound;
+                    }
+                }
+
+                GUI.enabled = lowerBoundValid && upperBoundValid;
+                if (GUILayout.Button("Force re-generate heatmap"))
+                {
+                    EditorUtility.DisplayProgressBar("Generating heatmap", "Generating position heatmap texture...",
+                        -1);
+                    _positionHeatmapManager.ForceRegenerate();
+                    EditorUtility.ClearProgressBar();
+                }
+
+                GUI.enabled = true;
+
+                if (GUILayout.Button("Export raw data"))
+                {
+                    _positionHeatmapManager.Export();
                 }
             }
 
-            GUI.enabled = lowerBoundValid && upperBoundValid;
-            if (GUILayout.Button("Force re-generate heatmap"))
-            {
-                EditorUtility.DisplayProgressBar("Generating heatmap", "Generating position heatmap texture...", -1);
-                _positionHeatmapManager.ForceRegenerate();
-                EditorUtility.ClearProgressBar();
-            }
-            GUI.enabled = true;
-            
-            if (GUILayout.Button("Export raw data"))
-            {
-                _positionHeatmapManager.Export();
-            }
-            
             GUILayout.EndVertical();
         }
     }
